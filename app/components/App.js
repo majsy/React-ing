cbb7f3e8c4fdb54d21432dacf019d0f51db3a248
Madeleine Auvinen <madeleine@labela.nl>
2017-04-10 15:59:14 +0200

    make api call to swapi add input field start building filter function map data items

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {items: []}
+    }
+
+    componentWillMount() {
+        fetch( 'http://swapi.co/api/people/?format=json' )
+            .then( response => response.json() )
+            .then( ({results: items}) => (this.setState({items})) )
+    }
+
+    filter(e) {
+       this.setState({filter: e.target.value})
+    }
+
     render() {
+        let items = this.state.items;
         return (
-            <h1>Hello world!!!</h1>
-        );
+            <div>
+                <input type="text"
+                onChange={this.filter.bind(this)} />
+                {items.map(item => <h4 key={item.name}>{item.name}</h4>)}
+            </div>
+        )
     }
 }
